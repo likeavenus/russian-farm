1e6a70e7b86e64369d51000bee0e412415565e13
Rafael <rafael_guliev94@list.ru>
2024-06-18 17:40:40 +0300

    feature/refactoring: add plugin

--- vite/config.dev.mjs
+++ vite/config.dev.mjs
@@ -1,17 +1,20 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 
 export default defineConfig({
-    base: './',
-    build: {
-        rollupOptions: {
-            output: {
-                manualChunks: {
-                    phaser: ['phaser']
-                }
-            }
+  base: "./",
+  build: {
+    outDir: "./docs",
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          phaser: ["phaser"],
         },
+      },
     },
-    server: {
-        port: 8080
-    }
+  },
+  server: {
+    port: 8080,
+  },
+  plugins: [basicSsl()],
 });
