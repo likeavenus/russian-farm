bc605ccd88c981261d1b5a3b0a9c66e853f34407
Rafael <rafael_guliev94@list.ru>
2024-07-02 21:39:39 +0300

    main: add Ssl; add hash router; deploy

--- vite/config.dev.mjs
+++ vite/config.dev.mjs
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: './',
-    plugins: [
-        react(),
-    ],
+    base: "./",
+    plugins: [react(), basicSsl()],
     server: {
-        port: 8080
-    }
-})
+        port: 8080,
+    },
+});
+
